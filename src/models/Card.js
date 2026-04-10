850319cf14232d06db19b0560f4623c2a7f45f9b
Mark <mark.carroll345@gmail.com>
2026-04-10 18:37:59 +0100

    Api

--- src/models/Card.js
+++ src/models/Card.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 
 const cardSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  cardtype: { type: String, required: true },
-  description: { type: String },
-  image: { type: String } // Store Base64 string here
+  manaCost: { type: String },
+  type: { type: String, required: true },
+  oracleText: { type: String },
+  imageUrl: { type: String }
 });
 
 // Important: module.exports
